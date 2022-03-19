29b3571a28a54bce4ff15c05172b41220ed26114
GuyBlumenthal <guy.blumenthal@outlook.com>
2022-03-19 01:30:09 -0400

    Switched priority for interrupts

--- STM32F303K6Tx/Core/Src/stm32f3xx_hal_msp.c
+++ STM32F303K6Tx/Core/Src/stm32f3xx_hal_msp.c
@@ -194,7 +194,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* CAN interrupt Init */
-    HAL_NVIC_SetPriority(CAN_RX0_IRQn, 3, 0);
+    HAL_NVIC_SetPriority(CAN_RX0_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(CAN_RX0_IRQn);
   /* USER CODE BEGIN CAN_MspInit 1 */
 
@@ -264,7 +264,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM7_CLK_ENABLE();
     /* TIM7 interrupt Init */
-    HAL_NVIC_SetPriority(TIM7_DAC2_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(TIM7_DAC2_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(TIM7_DAC2_IRQn);
   /* USER CODE BEGIN TIM7_MspInit 1 */
 
